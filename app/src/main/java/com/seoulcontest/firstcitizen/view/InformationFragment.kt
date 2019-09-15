e0d7c811639cb5f632dd115552f14b22c7b8f303
hudson <pshh0218@gmail.com>
2019-09-15 14:52:51 +0900

    add - mainViewModel Test

---
+++ app/src/main/java/com/seoulcontest/firstcitizen/view/InformationFragment.kt
@@ -0,0 +1,26 @@
+package com.seoulcontest.firstcitizen.view
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.seoulcontest.firstcitizen.R
+import com.seoulcontest.firstcitizen.databinding.FragmentInformationBinding
+
+class InformationFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
+        val binding : FragmentInformationBinding = DataBindingUtil.inflate(inflater,R.layout.fragment_information,container,false)
+
+        return binding.root
+    }
+
+}
---
+++ app/src/main/java/com/seoulcontest/firstcitizen/view/ListFragment.kt
@@ -0,0 +1,27 @@
+package com.seoulcontest.firstcitizen.view
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.seoulcontest.firstcitizen.R
+import com.seoulcontest.firstcitizen.databinding.FragmentListBinding
+
+class ListFragment : Fragment() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
+        val binding : FragmentListBinding = DataBindingUtil.inflate(inflater,R.layout.fragment_list,container,false)
+
+        return binding.root
+    }
+
+}
