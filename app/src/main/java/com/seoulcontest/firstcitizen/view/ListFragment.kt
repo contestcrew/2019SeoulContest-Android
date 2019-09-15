6f89783bb569bf1a539fc3ec0ad1047d9823b814
preludezdev <preludez.dev@gmail.com>
2019-09-15 17:03:33 +0900

    temp commit 2

--- app/src/main/java/com/seoulcontest/firstcitizen/view/ListFragment.kt
+++ app/src/main/java/com/seoulcontest/firstcitizen/view/ListFragment.kt
@@ -19,7 +19,6 @@ class ListFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 
         val binding : FragmentListBinding = DataBindingUtil.inflate(inflater,R.layout.fragment_list,container,false)
-
         return binding.root
     }
 
