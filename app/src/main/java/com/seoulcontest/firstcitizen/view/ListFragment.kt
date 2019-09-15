5de176ee38529c427b80c879e64bb7d8062a68fd
preludezdev <preludez.dev@gmail.com>
2019-09-15 16:36:43 +0900

    temp commit

--- app/src/main/java/com/seoulcontest/firstcitizen/view/ListFragment.kt
+++ app/src/main/java/com/seoulcontest/firstcitizen/view/ListFragment.kt
@@ -11,7 +11,6 @@ import com.seoulcontest.firstcitizen.databinding.FragmentListBinding
 
 class ListFragment : Fragment() {
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
