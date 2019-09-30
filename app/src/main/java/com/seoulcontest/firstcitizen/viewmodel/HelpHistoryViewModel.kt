7214d489666da62addbf44acf252e60aa6ce44ac
hudson <pshh0218@gmail.com>
2019-09-30 16:30:50 +0900

    last commit

--- app/src/main/java/com/seoulcontest/firstcitizen/viewmodel/HelpHistoryViewModel.kt
+++ app/src/main/java/com/seoulcontest/firstcitizen/viewmodel/HelpHistoryViewModel.kt
@@ -2,10 +2,14 @@ package com.seoulcontest.firstcitizen.viewmodel
 
 import androidx.databinding.ObservableField
 import com.seoulcontest.firstcitizen.data.vo.GetReportHistory
+import com.seoulcontest.firstcitizen.data.vo.Request
 
 class HelpHistoryViewModel {
 
     var reportHistory  = ObservableField<GetReportHistory>() // 제보 히스토리 정보를 가지고 있는 변수
+    var request = ObservableField<Request>() // 의뢰 정보를 가지고 있는 변수
+
+
 
     companion object {
         private var INSTANCE: HelpHistoryViewModel? = null
